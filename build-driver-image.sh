ab8fd140f819b9d0ea02d3b5733123e9447e387f
Vitaliy Emporopulo <vemporop@redhat.com>
2025-09-10 17:18:24 +0300

    Build multi-arch by default

--- build-driver-image.sh
+++ build-driver-image.sh
@@ -14,5 +14,6 @@ cd "${DRA_DRIVER_DIR}"
 export REGISTRY=${REGISTRY:-quay.io/vemporop}
 export IMAGE_TAG=$(git show --format=reference | head -1 | awk '{print $1}')
 export PUSH_ON_BUILD=${PUSH_ON_BUILD:-true}
+export BUILD_MULTI_ARCH_IMAGES=${BUILD_MULTI_ARCH_IMAGES:-true}
 
 make -f deployments/container/Makefile build
