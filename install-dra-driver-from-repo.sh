ddfaf1f6c1a0700692eefe48ee4c85b40ff7e6e9
Vitaliy Emporopulo <vemporop@redhat.com>
2025-09-30 20:30:01 +0300

    Bump DRA driver version to 25.3.2

--- install-dra-driver-from-repo.sh
+++ install-dra-driver-from-repo.sh
@@ -7,7 +7,7 @@ helm repo add nvidia https://helm.ngc.nvidia.com/nvidia \
     && helm repo update
 
 helm install nvidia-dra-driver-gpu nvidia/nvidia-dra-driver-gpu \
-    --version="25.3.1" \
+    --version="25.3.2" \
     --create-namespace \
     --namespace nvidia-dra-driver-gpu \
     --set nvidiaDriverRoot=/run/nvidia/driver \
