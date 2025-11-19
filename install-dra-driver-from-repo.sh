242a01f8640de93a7944431b95c0303dd60222b9
Vitaliy Emporopulo <vemporop@redhat.com>
2025-11-19 14:08:04 +0200

    Update chart version, add a SCC fix

--- install-dra-driver-from-repo.sh
+++ install-dra-driver-from-repo.sh
@@ -6,8 +6,9 @@ set -x
 helm repo add nvidia https://helm.ngc.nvidia.com/nvidia \
     && helm repo update
 
+# To use the latest version, remove the --version="25.8.0" line
 helm install nvidia-dra-driver-gpu nvidia/nvidia-dra-driver-gpu \
-    --version="25.3.2" \
+    --version="25.8.0" \
     --create-namespace \
     --namespace nvidia-dra-driver-gpu \
     --set nvidiaDriverRoot=/run/nvidia/driver \
