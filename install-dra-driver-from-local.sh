09e4afbcac3c60bc5e65b2179ddee31d2b822e65
Vitaliy Emporopulo <vemporop@redhat.com>
2025-12-09 20:05:10 +0200

    Clean up

--- install-dra-driver-from-local.sh
+++ install-dra-driver-from-local.sh
@@ -15,24 +15,17 @@ REGISTRY=${REGISTRY:-"nvcr.io/nvidia"}
 TAG=${TAG:-"v25.8.0"}
 IMAGE=${IMAGE:-"k8s-dra-driver-gpu"}
 FORCE_GPU_SUPPORT=${FORCE_GPU_SUPPORT:-false}
-COMPUTE_DOMAIN_TEST_MODE=${COMPUTE_DOMAIN_TEST_MODE:-false}
 
 echo "DRA driver directory: ${DRA_DRIVER_DIR}"
 echo "Image tag: ${TAG}"
 echo "Image registry: ${REGISTRY}"
 echo "Image name: ${IMAGE}"
 echo "Force GPU support: ${FORCE_GPU_SUPPORT}"
-echo "Compute domain test mode: ${COMPUTE_DOMAIN_TEST_MODE}"
 FORCE_GPU_SUPPORT_OPTIONS=""
 if [ "$FORCE_GPU_SUPPORT" = true ]; then
     FORCE_GPU_SUPPORT_OPTIONS="--set resources.gpus.enabled=true --set gpuResourcesEnabledOverride=true"
 else
     FORCE_GPU_SUPPORT_OPTIONS="--set resources.gpus.enabled=false"
-fi
-
-COMPUTE_DOMAIN_TEST_MODE_OPTIONS=""
-if [ "$COMPUTE_DOMAIN_TEST_MODE" = true ]; then
-    COMPUTE_DOMAIN_TEST_MODE_OPTIONS="--set resources.computeDomains.testingMode=true"
 fi
 
 # Override controller.affinity to allow scheduling on worker nodes
@@ -42,7 +35,6 @@ helm install nvidia-dra-driver-gpu ${DRA_DRIVER_DIR}/deployments/helm/nvidia-dra
     --namespace nvidia-dra-driver-gpu \
     --set nvidiaDriverRoot=/run/nvidia/driver \
     ${FORCE_GPU_SUPPORT_OPTIONS} \
-    ${COMPUTE_DOMAIN_TEST_MODE_OPTIONS} \
     --set image.tag=${TAG} \
     --set image.repository=${REGISTRY}/${IMAGE} \
     --set controller.affinity=null
